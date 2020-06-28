0557adf120944ff398f4a24ea0205afbc3222e8a
shashank1436 <9945shashank@gmail.com>
2020-06-29 02:50:07 +0530

    derp

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -311,6 +311,7 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 							!mfd->bl_level)) {
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, bl_lvl);
+		mutex_unlock(&mfd->bl_lock);
 #ifdef CONFIG_KLAPSE
                  set_rgb_slider(bl_lvl);
 #endif
